a56a84299f0c086d8c615cc397cefed3676e1c99
Lerte Smith <smith.shi@fantem.com>
2018-09-20 15:44:58 +0800

    update switch lang
    Signed-off-by: Lerte Smith <smith.shi@fantem.com>

--- static/ip2lang.js
+++ static/ip2lang.js
@@ -1,10 +1,6 @@
 if (!/^[1-6][0-7][0-3]\d0[0-4]$/.test(returnCitySN.cid)) {
   if (!/^\/en/.test(location.pathname)) {
-    location.href =
-      location.origin +
-      '/en' +
-      location.pathname +
-      location.search +
-      location.hash
+    location.href = location.origin + location.pathname.replace(/^\/en/, '/zh')
+    location.search + location.hash
   }
 }
