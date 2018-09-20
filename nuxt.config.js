28bc3918b80b28e5510b761683365963ab666a22
Lerte Smith <smith.shi@fantem.com>
2018-09-20 09:08:25 +0800

    fixed ip is not china location to english version
    Signed-off-by: Lerte Smith <smith.shi@fantem.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,20 +41,13 @@ export default {
     script: [
       { src: 'https://pv.sohu.com/cityjson' },
       {
-        innerHTML: `
-        if (!/^[1-6][0-7][0-3]\d0[0-4]$/.test(returnCitySN.cid)) {
-          if(location.pathname === '/') {
-            location.href = location.origin + '/en' + location.pathname + location.search + location.hash
-          }
-        }
-        `
+        src: '/ip2lang.js'
       },
       {
         src:
           'https://cdn.bootcss.com/smooth-scroll/12.1.5/js/smooth-scroll.polyfills.min.js'
       }
-    ],
-    __dangerouslyDisableSanitizers: ['script']
+    ]
   },
   /*
   ** Customize the progress-bar color
