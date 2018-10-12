7591302f5556162d7d3392d98d8a8d767804d140
Lerte Smith <smith.shi@fantem.com>
2018-10-12 08:50:13 +0800

    add if under ie11, go to update web browser page
    Signed-off-by: Lerte Smith <smith.shi@fantem.com>

--- static/nav2lang.js
+++ static/nav2lang.js
@@ -1,3 +1,9 @@
+var userAgent = navigator.userAgent
+var isIE =
+  userAgent.indexOf('compatible') > -1 && userAgent.indexOf('MSIE') > -1
+if (isIE) {
+  location = '/update-browser-web.html'
+}
 var lang = (navigator.language || navigator.browserLanguage).toLowerCase()
 if (!/zh/.test(lang)) {
   if (!/^\/en/.test(location.pathname)) {
