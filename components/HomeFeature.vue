ae6988e2a6eccd9b97ddc29248d1d1735d1161a2
Lerte Smith <smith.shi@fantem.com>
2018-10-19 15:58:56 +0800

    update HomeFeature
    Signed-off-by: Lerte Smith <smith.shi@fantem.com>

--- components/HomeFeature.vue
+++ components/HomeFeature.vue
@@ -1,18 +1,18 @@
 <template>
   <section id="section-feature">
-    <div class="columns">
-      <div class="column">
-        <div class="container has-text-centered">
-          <p class="title">Tap-and-Touch {{$t('专利技术')}}</p>
-          <p class="subtitle">{{$t('让组网设置超乎想象的简单')}}</p>
-          <p class="content">
-            {{$t('OOMI使用自主创新的Tap-and-Touch专利技术，刷新智能家居安装新定义：只需让OOMI Touch轻触其他OOMI产品，便可建立链接，实现OOMI系统对智能家居的全操控。智能家居生活，从触碰开始。')}}            
-          </p>
+    <div id="tap-and-touch" class="container">
+      <div class="columns">
+        <div class="column">
+          <div class="has-text-centered">
+            <p class="title">Tap-and-Touch {{$t('专利技术')}}</p>
+            <p class="subtitle">{{$t('让组网设置超乎想象的简单')}}</p>
+            <p class="content">
+              {{$t('OOMI使用自主创新的Tap-and-Touch专利技术，刷新智能家居安装新定义：只需让OOMI Touch轻触其他OOMI产品，便可建立链接，实现OOMI系统对智能家居的全操控。智能家居生活，从触碰开始。')}}            
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="columns">
-      <div class="container columns" style="margin: 0 auto">
+      <div class="columns">
         <div class="column">
           <div class="num center">1</div>
           <figure class="center">
@@ -37,19 +37,19 @@
       </div>
     </div>
     <div class="gap"></div>
-    <div class="columns">
-      <div class="column">
-        <div class="container has-text-centered">
-          <p class="title">OOMI3.0{{$t('大宅系统')}}</p>
-          <p class="subtitle">{{$t('让组网设置超乎想象的简单')}}</p>
-          <p class="content">
-            {{$t('OOMI大宅系统涵盖健康、语音、安防、灯光、家电、娱乐、环境七大类产品，采用边缘计算技术，可脱离云服务区独立运行，确保在网络中断的情况下智能家居系统依然可以正常工作。该系统运行稳定，将彻底解决无线产品在别墅、复式、大平台中应用的稳定性难题。OOMI系统支持多网关协同工作，支持跨网关场景联动。用户通过一个账号即可集中管控，也可以使用不同的账号进行分级管控，满足了所有家庭成员的使用以及大型系统的应用。')}}            
-          </p>
+    <div id="oomi3-big-house" class="container">
+      <div class="columns">
+        <div class="column">
+          <div class="container has-text-centered">
+            <p class="title">OOMI3.0{{$t('大宅系统')}}</p>
+            <p class="subtitle">{{$t('让组网设置超乎想象的简单')}}</p>
+            <p class="content">
+              {{$t('OOMI大宅系统涵盖健康、语音、安防、灯光、家电、娱乐、环境七大类产品，采用边缘计算技术，可脱离云服务区独立运行，确保在网络中断的情况下智能家居系统依然可以正常工作。该系统运行稳定，将彻底解决无线产品在别墅、复式、大平台中应用的稳定性难题。OOMI系统支持多网关协同工作，支持跨网关场景联动。用户通过一个账号即可集中管控，也可以使用不同的账号进行分级管控，满足了所有家庭成员的使用以及大型系统的应用。')}}            
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="columns">
-      <div class="container columns" style="margin: 0 auto">
+      <div class="columns">
         <div class="column is-10">
           <div class="center">
             <img :src="`${this.$store.state.cdn}/2018/09/c3bd8a49f94e8f1f641e78eb0e6a8d90.png`">
@@ -76,13 +76,20 @@
     background-color: #f0f4f5;
   }
 
-  > .columns {
-    &:nth-child(1), &:nth-child(4) {
-      padding-top: 60px;
+  .container {
+    .columns {
+      padding: 20px 0;
+
+      &:nth-child(1) {
+        padding-top: 60px;
+      }
+
+      &:nth-child(2) {
+        padding-bottom: 60px;
+      }
     }
 
-    &:nth-child(2), &:nth-child(5) {
-      padding: 38px 0;
+    &.oomi3 {
     }
 
     .title {
