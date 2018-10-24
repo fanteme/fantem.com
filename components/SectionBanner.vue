38a690ad6cf8a7e6b1c42be4a8112d7f2f99db8a
shaoting <shaoting@fantem.com>
2018-10-24 16:47:30 +0800

    add business page

---
+++ components/SectionBanner.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="main">
+    <section class="sectionbanner">
+      <div class="hero center" :style="{backgroundImage: `url(${$store.state.cdn + banner.backgroundimg})`}">
+        <div class="container-fluid has-text-centered">
+          <div v-html="banner.icon"></div>
+          <h2 class="caption has-text-weight-semibold">
+            {{banner.title}}
+          </h2>
+          <p class="subtitle">{{banner.subtitle}}</p>
+          <a v-if="banner.buttontxt" class="button">{{banner.buttontxt}}</a>
+        </div>
+      </div>
+    </section>
+  </main>  
+</template>
+<style lang="stylus">
+  .sectionbanner
+    color: #fff
+    padding-bottom: 24px
+    
+    .hero
+      height: 701px 
+      .caption
+        font-size: 36px
+        line-height: 44px
+        margin: 35px 0
+      .subtitle
+        color: #fff 
+        font-size: 28px 
+        margin: 49px 0 73px 0 
+      .button 
+        width: 240px
+        height: 57px
+        font-size: 18px
+        color: #fff
+        background: #e76c26
+        border: none
+</style>
+<script>
+  export default {
+    props: {
+      banner: {
+        type: Object
+      }
+    }
+  }
+</script>
