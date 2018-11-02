e9ebedf6f7588b2d4287dc6ea599fc7436e66885
shaoting <shaoting@fantem.com>
2018-11-02 11:14:17 +0800

    update google map to https
    Signed-off-by: shaoting <shaoting@fantem.com>

--- pages/_lang/about/contact.vue
+++ pages/_lang/about/contact.vue
@@ -22,7 +22,7 @@
         </div>
         <div class="columns">
           <div class="column">
-            <iframe width='100%' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='http://www.google.cn/maps/embed?pb=!1m18!1m12!1m3!1d3685.183871143453!2d113.94425131540099!3d22.534783940035815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3403ee04b9b0bc07%3A0x756b906bd375f3fd!2z6Iux5ZSQ5aSn5Y6m!5e0!3m2!1szh-CN!2scn!4v1537494459852'></iframe>
+            <iframe width='100%' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='//www.google.cn/maps/embed?pb=!1m18!1m12!1m3!1d3685.183871143453!2d113.94425131540099!3d22.534783940035815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3403ee04b9b0bc07%3A0x756b906bd375f3fd!2z6Iux5ZSQ5aSn5Y6m!5e0!3m2!1szh-CN!2scn!4v1537494459852'></iframe>
           </div>
         </div>
       </div>
