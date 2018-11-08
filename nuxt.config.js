6d39403b05f7f4e7904fe0a052cbeb79ad876ead
shaoting <shaoting@fantem.com>
2018-11-08 11:53:14 +0800

    optimise the config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,7 @@
-import minimist from 'minimist'
-const argv = minimist(process.argv.slice(2), {
-  alias: {
-    H: 'hostname',
-    p: 'port'
-  },
-  string: ['H'],
-  unknown: parameter => false
-})
 
-const port =
-  argv.port ||
-  process.env.PORT ||
-  process.env.npm_package_config_nuxt_port ||
-  '3000'
-const host =
-  argv.hostname ||
-  process.env.HOST ||
-  process.env.npm_package_config_nuxt_host ||
-  'localhost'
 
 export default {
-  env: {
-    baseUrl: process.env.BASE_URL || `http://${host}:${port}`
-  },
+
   head: {
     title: '官网首页',
     htmlAttrs: {
@@ -179,24 +158,6 @@ export default {
     extend(config, ctx) {
       if (ctx.isClient) {
         config.devtool = 'eval-source-map'
-      }
-      const tsLoader = {
-        loader: 'ts-loader',
-        options: { appendTsSuffixTo: [/\.vue$/], transpileOnly: true }
-      }
-      config.module.rules.push({
-        test: /((client|server)\.js)|(\.tsx?)$/,
-        ...tsLoader
-      })
-      config.module.rules.map(rule => {
-        if (rule.loader === 'vue-loader') {
-          rule.options.loaders = { ts: tsLoader }
-        }
-        return rule
-      })
-      // Add .ts extension in webpack resolve
-      if (config.resolve.extensions.indexOf('.ts') === -1) {
-        config.resolve.extensions.push('.ts')
       }
     }
   }
