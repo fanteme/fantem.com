36c57458f255e0d568bc6dda799ef95574898c13
Lerte Smith <smith.shi@fantem.com>
2019-01-31 15:14:44 +0800

    add console info

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,22 +2,25 @@
   <footer class="footer">
     <div class="container">
       <div class="columns">
-        <div class="column is-8"> 
+        <div class="column is-8">
           <section class="columns is-multiline is-mobile">
-              <ul class="column is-half-mobile" :key="index" v-for="(menu, index) of menus">
-                <h2 class="caption">{{menu.title}}</h2>
-                <li v-for="(item, index) of menu.menus" :key="index">
-                  <nuxt-link :to="item.link" v-text="item.title"></nuxt-link>
-                </li>
-              </ul>
-            </section>
+            <ul class="column is-half-mobile" :key="index" v-for="(menu, index) of menus">
+              <h2 class="caption">{{menu.title}}</h2>
+              <li v-for="(item, index) of menu.menus" :key="index">
+                <nuxt-link :to="item.link" v-text="item.title"></nuxt-link>
+              </li>
+            </ul>
+          </section>
         </div>
         <div class="column is-4 contact has-text-right has-text-left-mobile">
           <ul>
             <li class="tel has-text-weight-semibold">400-880-3757</li>
             <li class="wechat-mp">
               <div class="qrcode">
-                <img width="85" :src="`${this.$store.state.cdn}/2018/11/ce44235d3aaeff8387bb0307ead2b106.png`">
+                <img
+                  width="85"
+                  :src="`${this.$store.state.cdn}/2018/11/ce44235d3aaeff8387bb0307ead2b106.png`"
+                >
               </div>
               <span>{{$t('关注我们')}}</span>
             </li>
@@ -25,23 +28,54 @@
         </div>
       </div>
       <div class="columns">
-        <div class="column"><hr/></div>
+        <div class="column">
+          <hr>
+        </div>
       </div>
       <div class="columns">
         <div class="column has-text-left company">
-          ©2018 {{$t('丰唐物联技术（深圳）有限公司')}}<a target="_blank" href="http://www.miitbeian.gov.cn" class="icp">粤ICP备14021056号-1</a>
+          ©2018 {{$t('丰唐物联技术（深圳）有限公司')}}
+          <a
+            target="_blank"
+            href="http://www.miitbeian.gov.cn"
+            class="icp"
+          >粤ICP备14021056号-1</a>
         </div>
         <div class="column has-text-right">
           <ul class="is-inline-flex link">
-            <li><a @click.prevent="switchLang('zh')">中文</a></li>
-            <li><span>|</span></li>
-            <li><a @click.prevent="switchLang('en')">ENG</a></li>
-          </ul>  
+            <li>
+              <a @click.prevent="switchLang('zh')">中文</a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a @click.prevent="switchLang('en')">ENG</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
     <div class="go2top center" v-show="isDisplay2Top" :title="$t('返回顶部')">
-      <svg @click="go2top" width="22" height="23" viewBox="0 0 22 23" xmlns="http://www.w3.org/2000/svg"><g fill-rule="nonzero" fill="none"><path d="M21 12.5c-.3 0-.5-.1-.7-.3l-9-9.2-9 9.2c-.4.4-1 .4-1.4 0-.4-.4-.4-1 0-1.4l9.7-9.9c.4-.4 1.1-.4 1.4 0l9.7 9.9c.4.4.4 1 0 1.4-.2.2-.5.3-.7.3z" id="Shape" fill="#3E3A39"/><path d="M17.4 22.5h-12.4c-.6 0-1-.4-1-1s.4-1 1-1h12.4c.6 0 1 .4 1 1s-.5 1-1 1z" fill="#EE7623"/></g></svg>
+      <svg
+        @click="go2top"
+        width="22"
+        height="23"
+        viewBox="0 0 22 23"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g fill-rule="nonzero" fill="none">
+          <path
+            d="M21 12.5c-.3 0-.5-.1-.7-.3l-9-9.2-9 9.2c-.4.4-1 .4-1.4 0-.4-.4-.4-1 0-1.4l9.7-9.9c.4-.4 1.1-.4 1.4 0l9.7 9.9c.4.4.4 1 0 1.4-.2.2-.5.3-.7.3z"
+            id="Shape"
+            fill="#3E3A39"
+          ></path>
+          <path
+            d="M17.4 22.5h-12.4c-.6 0-1-.4-1-1s.4-1 1-1h12.4c.6 0 1 .4 1 1s-.5 1-1 1z"
+            fill="#EE7623"
+          ></path>
+        </g>
+      </svg>
     </div>
   </footer>
 </template>
@@ -50,7 +84,7 @@
 footer.footer {
   padding: 4rem 1.5rem 3rem;
   background: #fff;
-  box-shadow: 0 2px 10px rgba(0,0,0,0.4);
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.4);
 
   .company {
     font-size: 12px;
@@ -115,7 +149,7 @@ footer.footer {
     position: fixed;
     right: 48px;
     bottom: 96px;
-    background: rgba(255,255,255,0.4);
+    background: rgba(255, 255, 255, 0.4);
   }
 }
 </style>
@@ -257,6 +291,15 @@ export default {
         this.isDisplay2Top = false
       }
     })
+    console.info(`
+ ________    ________      ________      _________     _______       _____ ______      
+|\\  _____\\  |\\   __  \\    |\\   ___  \\   |\\___   ___\\  |\\  ___ \\     |\\   _ \\  _   \\    
+\\ \\  \\__/   \\ \\  \\|\\  \\   \\ \\  \\\\ \\  \\  \\|___ \\  \\_|  \\ \\   __/|    \\ \\  \\\\\\__\\ \\  \\   
+ \\ \\   __\\   \\ \\   __  \\   \\ \\  \\\\ \\  \\      \\ \\  \\    \\ \\  \\_|/__   \\ \\  \\\\|__| \\  \\  
+  \\ \\  \\_|    \\ \\  \\ \\  \\   \\ \\  \\\\ \\  \\      \\ \\  \\    \\ \\  \\_|\\ \\   \\ \\  \\    \\ \\  \\ 
+   \\ \\__\\      \\ \\__\\ \\__\\   \\ \\__\\\\ \\__\\      \\ \\__\\    \\ \\_______\\   \\ \\__\\    \\ \\__\\
+    \\|__|       \\|__|\\|__|    \\|__| \\|__|       \\|__|     \\|_______|    \\|__|     \\|__|
+    `)
   }
 }
 </script>
